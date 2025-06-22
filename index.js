8e666b7d76e1239178beeb87f7578b1f7581757f
poq <120293384+poqbox@users.noreply.github.com>
2025-06-21 20:13:08 -0400

    Added route handling for the example sandboxes collection

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
-import collections from "./routes/collections.js"
 import {get_database_URI} from "./utils/utils.js"
+import collections from "./routes/collections.js"
+import sandbox from "./routes/sandbox.js"
 
 
 const PORT = 3000
-const database_name = "sample_training"
+const database_name = "sandbox"
 await mongoose.connect(get_database_URI(database_name))
 const app = express()
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}.`) })
+app.use(express.json())
 
 
 // routers
 app.use("/api/collections", collections)
+app.use("/api/sandbox", sandbox)
 
 
 // routes
---
+++ routes/sandbox.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mongoose from "mongoose"
+import error from "../utils/error.js"
+import Sandbox from "../models/sandbox.js"
+import Sand from "../models/sand.js"
+import Stuff from "../models/stuff.js"
+
+
+const router = express.Router()
+
+
+// routes
+router.route("/")
+  .get(async (request, response) => {
+    response.json(await Sandbox.find(request.body))
+  })
+  .post(async (request, response, next) => {
+    const name = request.body.name
+    const length = request.body.length
+    const width = request.body.width
+    const height = request.body.height
+    const stuff = request.body.stuff
+
+    if (name && length && width && height) {
+      const sandbox = new Sandbox({
+        name: name,
+        size: {
+          length: length,
+          width: width,
+          height: height
+        }
+      })
+      if (stuff) {sandbox.stuff = stuff}
+        await sandbox.save()
+
+      response.json(sandbox)
+    }
+    else {
+      next(error(400, "Insufficient data."))
+    }
+  })
+
+router.route("/:id")
+  .get(async (request, response) => {
+    response.json(await Sandbox.findById(request.params.id))
+  })
+export default router
